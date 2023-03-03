b94231d79ced58b61b18003923e873285bd67ab5
Nahuel <nahuelarancibia4@gmail.com>
2023-03-03 16:31:44 -0300

    pruebaSegundoCmmit

---
+++ Practica0/ejercicio4.pas
@@ -0,0 +1,13 @@
+
+(*Implemente un programa que lea el diámetro D de un círculo e imprima:
+a. El radio (R) del círculo (la mitad del diámetro)
+b. El área del círculo. Para calcular el área de un círculo debe utilizar la fórmula PI x R
+2
+c. El perímetro del círculo. Para calcular el perímetro del círculo debe utilizar la fórmula
+D*PI (o también PI*R*2)
+*)
+
+Program ejercicio4;
+Begin
+  
+End.
