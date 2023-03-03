5eb23647a0772a8e72cfcd5e0ac2ef04a06b61e7
Nahuel <nahuelarancibia4@gmail.com>
2023-03-03 16:40:15 -0300

    prueba3

--- Practica0/ejercicio4.pas
+++ Practica0/ejercicio4.pas
@@ -1,4 +1,5 @@
 
+
 (*Implemente un programa que lea el diámetro D de un círculo e imprima:
 a. El radio (R) del círculo (la mitad del diámetro)
 b. El área del círculo. Para calcular el área de un círculo debe utilizar la fórmula PI x R
@@ -8,6 +9,16 @@ D*PI (o también PI*R*2)
 *)
 
 Program ejercicio4;
+
+Var 
+  diametro,radio,area,perimetro: real;
 Begin
-  
+  Writeln('Ingrese el diametro del circulo: ');
+  Readln(diametro);
+  radio := diametro/2;
+  area := pi*radio*radio;
+  perimetro := diametro*pi;
+  Writeln('El radio del circulo es: ',radio:2:2);
+  Writeln('El area del circulo es: ',area:2:2);
+  Writeln('El perimetro del circulo es: ',perimetro:2:2);
 End.
