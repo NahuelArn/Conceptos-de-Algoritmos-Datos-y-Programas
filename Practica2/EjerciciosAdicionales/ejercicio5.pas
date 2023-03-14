6f788096bd93dee7ac5c935ebd538bed6286cc0d
Nahuel <nahuelarancibia4@gmail.com>
2023-03-14 15:07:06 -0300

    back

--- Practica2/EjerciciosAdicionales/ejercicio5.pas
+++ Practica2/EjerciciosAdicionales/ejercicio5.pas
@@ -1,3 +1,4 @@
+
 { 5. En la “Práctica 1 - Ejercicios Adicionales” se resolvieron 3 problemas complejos sin utilizar módulos. Al
 carecer de herramientas para modularizar, esos programas resultaban difíciles de leer, de extender y de
 depurar.
@@ -12,13 +13,19 @@ anterior
 }
 
 //Practica 1 - Ejercicio 1
+
 {
  - Modularizado: leer datos, mayor inversor
   en este caso no se vio necesario el anidamiento de modulos
  - se utilizo 2 procedimiento, ya que se tenia que retornar para utilizar dats en el programa principal
 }
 ////Practica 1 - Ejercicio 2
-  {
-    -
+
+{
+    -Se obtubo algunas ventajas al usar modularizacion y anidacion en modulos pero, los parametros se vuelven bastantes
+    - se deberia reestructura mejor cosa de que queden los parametros distribuidos de forma mas equitativa
   }
 //Practica 1 - Ejercicio 3
+{
+  -Este quedaria mejor y mas limpio el codigo con modularizacion y anidacion de modulos
+}
