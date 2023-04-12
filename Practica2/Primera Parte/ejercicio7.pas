8fdc0a30b549893ee380e87c6bc94b940173bda9
Nahuel <nahuelarancibia4@gmail.com>
2023-04-12 20:19:17 -0300

    eje3TP4par2

--- Practica2/Primera Parte/ejercicio7.pas
+++ Practica2/Primera Parte/ejercicio7.pas
@@ -1,5 +1,17 @@
 // 7. Dado el siguiente programa: alcanceYFunciones
 
+(*
+a) La función calcularPromedio calcula y retorna el promedio entre las variables globales suma y cant, pero
+parece incompleta. ¿qué debería agregarle para que funcione correctamente?
+b) En el programa principal, la función calcularPromedio es invocada dos veces, pero esto podría mejorarse.
+¿cómo debería modificarse el programa principal para invocar a dicha función una única vez?
+c) Si se leen por teclado los valores 48 (variable suma) y 6 (variable cant), ¿qué resultado imprime el
+programa? Considere las tres posibilidades:
+i) El programa original (directamente no compila)
+ii) El programa luego de realizar la modificación del inciso a)  (imprime el mensaje -1 y te tira el mensaje de noes es buen idea div x 0, pero llama 2 veces a la función)
+iii) El programa luego de realizar las modificaciones de los incisos a) y b) (imprime 8 de promedio)
+*)
+
 Program ejercicio7;
 
 Var 
--- Practica4/Segunda Parte/ejercicio3.pas
+++ Practica4/Segunda Parte/ejercicio3.pas
@@ -29,6 +29,13 @@ Type
     montoDinero: real;
     distanciaRecorrida: real;
   End;
+{--------------------Item=C------------------------------}
+  //no necesito incializarlo, necesito las posiciones cargadas y su dimL
+  vHijoIdAliminar = array [1..dimFr] Of integer;
+
+  vPadreIdAliminar = array[1..31] Of integer;
+
+{---------------------------------------------------}
 {--------------------------------------------------}
   //vector contador que guarda dimesiones logicas.
   vLogico = array[1..rangoMes] Of integer;
@@ -98,6 +105,105 @@ Begin
     End;
 End;
 
+{b. Realizar un módulo que reciba el vector generado en a) e informe:
+- El monto promedio transportado de los viajes realizados
+- La distancia recorrida y el día del mes en que se realizó el viaje que transportó menos dinero.
+- La cantidad de viajes realizados cada día del mes.
+Nota: para realizar el inciso b, el vector debe recorrerse una única vez}
+Function promedio(suma: real; cant: integer): real;
+Begin
+  promedio := suma/cant;
+End;
+
+Function validadorLow(montoActual: real;min: real): boolean;
+Begin
+  validadorLow := montoActual < min;
+End;
+
+Procedure recorrerVector(vv: vvMes; vL2:vLogico);
+
+Var 
+  i,j,dimL,cantTotalViajes: integer;
+  sumaDistanciaRecorrida,sumaPromViajes: real;
+  diaLow: integer;
+  min: real;
+Begin
+  cantTotalViajes := 0;
+  sumaPromViajes := 0;
+  sumaDistanciaRecorrida := 0;
+  min := 9999;
+  For i:= 1 To rangoMes Do
+    Begin
+      dimL := vL2[i];
+      For j:= 1 To dimL Do
+        Begin
+          sumaPromViajes := sumaPromViajes + vv[i][j].montoDinero;
+          sumaDistanciaRecorrida := sumaDistanciaRecorrida+ vv[i][j].distanciaRecorrida;
+          If (validadorLow(vv[i][j].montoDinero,min))Then
+            Begin
+              min := vv[i][j].montoDinero;
+              diaLow := i;
+            End;
+          Writeln('La cantidad de viajes realizados el dia, ',i, ' son: ',dimL);
+        End;
+      cantTotalViajes := cantTotalViajes+ dimL;
+    End;
+  Writeln('El monto promedio transportado en todos los viajes realizados es: ',promedio(sumaPromViajes,cantTotalViajes): 2: 2);
+  Writeln('La distancia total recorrida es: ',sumaDistanciaRecorrida,' y el dia del mes en se transporto menos dinero es: ',diaLow);
+End;
+{c. Realizar un módulo que reciba el vector generado en a) y elimine todos los viajes cuya distancia
+recorrida sea igual a 100 km.}
+//
+
+{yo debiria tner un arreglo anteriormente, de arreglos, q me vaya tirando las posiciones en donde se cargo x distancia a eliminar
+Asi tendria un vv donde por dia tenga las posiciones a eliminar y pasarle a este modulo directamete el arreglo de viajes y otro arreglo con la data de las posiciones
+a eliminar...
+No me rinde hacer un vv don dimLs, ya que al eliminar el primer elemento la posicion tambien se va a ver afectada -1, al pedo hacer esto..
+recibo el modulo y lo recorro todas las veces q sean necesarias.. elimino y vuelvo a recorrerlo si no esta paso al dia siguiente 
+}
+Procedure eliminarPosv(Var ok: Boolean;Var dimL: integer; pos: integer;Var v: vvMes; dia: integer);
+//adapta este modulo a lo q queres
+
+Var 
+  j: integer;
+Begin
+  ok := false;
+  If ((pos>=1) And (pos<=dimL))Then
+    Begin
+      For j:= pos To (dimL-1) Do
+        v[dia][j] := v[dia][j+1];
+      ok := true;
+      dimL := dimL - 1;
+    End;
+End;
+//recorro el array cargado y por cada ocurrencia, elimino y vuelvo a empezar desde el principio
+//asi no me jode el corrimiento de la dimL
+Procedure eliminarViajesCortos(Var vv: vvMes; Var vL2: vLogico);
+
+Var 
+  i,j,dimL: integer;
+  ok: Boolean;
+  dia: integer;
+Begin
+  For i:=  1 To 31 Do
+    Begin
+      dimL := vL2[i];
+      j := 0;
+      While {(dimL < dimFr) And} (j < dimL) Do
+        Begin
+          j := j+1;
+          If (vv[i][j].distanciaRecorrida = 100)Then
+            Begin
+              dia := i;
+              {le estoy mandando un registro a un entero pero como le mando la pos sin romper}
+              eliminarPosv(ok,vL2[i],j{vv[i][j]},vv,dia);
+              dimL := vL2[i];
+              j := 0;
+            End;
+        End;
+    End;
+End;
+
 Procedure procesarDatos(Var v: vvMes);
 
 Var 
@@ -105,7 +211,9 @@ Var
   vv: vvMes;
 Begin
   cargarDatos(vv,vL2);
-  imprimirVv(vv,vL2);
+  // imprimirVv(vv,vL2);
+  recorrerVector(vv,vL2);
+  eliminarViajesCortos(vv,vL2);
 End;
 
 Var 
