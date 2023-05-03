f8b47299edb0b30978a6727b70b49c6a9868f789
Nahuel <nahuelarancibia4@gmail.com>
2023-05-02 21:53:11 -0300

    codVariacions

--- Practica4/Primera Parte/ejercicio14.pas
+++ Practica4/Primera Parte/ejercicio14.pas
@@ -117,7 +117,7 @@ Begin
       leerDatos(r);
       While (r.codProyecto <> -1) Do
         Begin
-          v[r.codProyecto] := r; {lo guardo para nada, directamente no me sirve ese vector}
+          // v[r.codProyecto] := r; {lo guardo para nada, directamente no me sirve ese vector}
           If (r.paisR = 'Argentina')Then
             montoArg := montoArg + tabla(r.cantHsLaburadas,r.rol);
           If (r.rol = 3)Then
@@ -179,6 +179,92 @@ End.
 
 
 
+
+//---------------
+-----------------------------
+Ejercicio 14
+program Algo;
+{ El repositorio de código fuente más grande en la actualidad, GitHub, desea
+estimar el monto invertido en los proyectos que aloja. Para ello, dispone de una
+tabla con información de los desarrolladores que participan en un proyecto de
+software, junto al valor promedio que se paga por hora de trabajo:
+Realizar un programa que procese la información de los desarrolladores que participaron en los 1000
+proyectos de software más activos durante el año 2017. De cada participante se conoce su país de
+residencia, código de proyecto (1 a 1000), el nombre del proyecto en el que participó, el rol que cumplió en
+dicho proyecto (1 a 5) y la cantidad de horas trabajadas.
+La lectura finaliza al ingresar el código de proyecto
+-1, que no debe procesarse. Al finalizar la lectura, el programa debe informar:
+a) El monto total invertido en desarrolladores con residencia en Argentina.
+b) La cantidad total de horas trabajadas por Administradores de bases de datos.
+c) El código del proyecto con menor monto invertido.
+d) La cantidad de Arquitectos de software de cada proyecto.
+}
+const
+  CANT_PROYECTOS = 1000;
+type
+    sCodigo = 1 .. CANT_PROYECTOS;
+    sRol = 1..5;
+
+    desarrollador = record
+      pais : string;
+      codigo_proyecto : sCodigo;
+      nombre : string;
+      rol : sRol;
+      horas_trabajadas : integer;
+    end;
+
+    vPrecios = array[sRol] of real;
+    vMontos = array[sCodigo] of real;
+    vArq = array[sCodigo] of real;
+
+procedure leerDesarrollador(var d:desarrollador);
+begin
+  readln(d.codigo_proyecto);
+  if (d.codigo_proyecto <> -1) do begin
+     readln(d.pais);
+     readln(d.nombre);
+     readln(d.rol);
+     readln(d.horas_trabajadas);
+  end;
+end;
+
+var
+  act : desarrollador;
+  precios : vPrecios;
+  montos : vMontos;
+  arq : vArq;
+
+  totalDesarrolladores : real;
+  horasBBDD : integer;
+begin
+
+  cargarPrecios(precios); //Se dispone
+  inicializarMontos(montos);  //Implementar
+  iniciarArq(arq);             //Implementar
+
+  totalDesarrolladores := 0;
+  horasBBDD := 0;
+
+  leerDesarrollador(act);
+  while (act.codigo_proyecto <> -1) do begin
+    if (act.pais = 'Argentina') then
+       totalDesarrolladores := totalDesarrolladores + act.horas_trabajadas * precios[act.rol];
+
+    if (act.rol = 3) then
+       horasBBDD := horasBBDD + act.horas_trabajadas;
+
+    montos[act.codigo_proyecto] = montos[act.codigo_proyecto] + act.horas_trabajadas * precios[act.rol];
+
+    if (act.rol = 4) then
+       arq[act.codigo_proyecto] = arq[act.codigo_proyecto] + 1;
+
+    leerDesarrollador(act);
+  end;
+
+  calcularMinimo(montos);   //Implementar
+  informarArquitectos(arq);  //Implementar
+
+end.  
 
 
 
--- Practica4/Primera Parte/ejercicio3.pas
+++ Practica4/Primera Parte/ejercicio3.pas
@@ -138,3 +138,66 @@ dimf de 10
 x=6
 y= 8
 }
+
+
+
+
+
+
+//mat
+P4 E3
+program sumador;
+const
+  dimF = 100;
+type
+    vNum = array [1..dimF] of integer;
+
+{Realizar un módulo que reciba el vector, una posición X y otra posición Y,
+e imprima el vector desde la posición X hasta la Y. Asuma que tanto X como Y son
+menores o igual a la dimensión lógica. Y considere que, dependiendo de los valores
+de X e Y, podría ser necesario recorrer hacia adelante o hacia atrás.}
+procedure imprimirXY (v : vNum; X : integer; Y : integer);
+var
+  i : integer;
+begin
+  if (X < Y) then begin
+    for i := X to Y do begin
+      write('[ ', v[i], ' ] ');
+    end;
+  end
+  else begin
+    for i := X downto Y do begin
+      write('[ ', v[i], ' ] ');
+    end;
+  end;
+  writeln();
+end;
+
+var
+   v : vNum;
+   dimL : integer;
+   i : integer;
+begin
+  dimL := 0;
+  for i:= 1 to 10 do
+      v[i] := i;
+  dimL := 10;
+
+  //inciso A
+  writeln('Inciso A : ');
+  imprimirXY(v, 1, dimL);
+
+  //inciso B
+  writeln('Inciso B : ');
+  imprimirXY(v, dimL, 1);
+
+  //inciso C
+  writeln('Inciso C : ');
+  imprimirXY(v, dimL DIV 2, 1);
+  imprimirXY(v, dimL DIV 2 + 1, dimL);
+
+  readln();
+  readln();
+
+end.
+----------------------------------------
